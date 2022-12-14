f8cbb462f96940116f592cb8339b994e826e43f2
Gleb V. Zhegulin <102049907+ProfessorNemo@users.noreply.github.com>
2022-12-14 23:12:40 +0300

    Update events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# (с) goodprogrammer.ru
-#
 # Контроллер, управляющий событиями
 class EventsController < ApplicationController
   protect_from_forgery except: :show
